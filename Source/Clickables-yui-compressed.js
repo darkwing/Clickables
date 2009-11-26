ef4861de0f1fe3b27344f52195d97a1028185c9d
David Walsh <davidwalsh83@gmail.com>
2009-11-25 20:10:52 -0600

    Adding YAML headers

--- Source/Clickables-yui-compressed.js
+++ Source/Clickables-yui-compressed.js
@@ -1,10 +1,18 @@
 /*
-Script: Clickables
+---
+description:     Clickables
 
-License: MIT-style license.
+authors:
+  - David Walsh (http://davidwalsh.name)
 
-Copyright: Copyright (c) 2007-2009 [David Walsh](http://davidwalsh.name/).
+license:
+  - MIT-style license
 
-Author: David Walsh (http://davidwalsh.name)
+requires:
+  core/1.2.1:   '*'
+
+provides:
+  - Clickables
+...
 */
 var Clickables=new Class({Implements:[Options],options:{elements:"li",selectClass:"",anchorToSpan:false},initialize:function(a){this.setOptions(a);this.elements=$$(this.options.elements);this.doClickables();},doClickables:function(){this.elements.each(function(c){var a=c.getElements("a"+(this.options.selectClass?"."+this.options.selectClass:""))[0];if($defined(a)){this.setClick(c,a.get("href"));if(this.options.anchorToSpan){var b=new Element("span",{text:a.get("text")}).replaces(a);}}},this);},setClick:function(b,a){b.addEvent("click",function(){window.location=a;});}});
